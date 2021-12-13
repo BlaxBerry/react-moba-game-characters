94aefe4be657b6df6e1a8dea99dd2e32983fb608
陳嘉旭 <73395592+BlaxBerry@users.noreply.github.com>
2021-12-13 16:34:19 +0900

    set http proxy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,32 +1,33 @@
+import axios from 'axios'
 import instance from './request'
 
 // get all hero
 export const getHeroList = () => {
-    return instance.get('https://autumnfish.cn/api/lol/search')
+    return instance.get('/search')
 }
 
 // search hero
 export const searchHero = (name: string) => {
-    return instance.get(`https://autumnfish.cn/api/lol/search?q=${name}`)
+    return instance.get(`search?q=${name}`)
 }
 
 // hero detail
 export const heroDetail = (id: string) => {
-    return instance.get(`https://autumnfish.cn/api/lol/info?id=${id}`)
+    return instance.get(`info?id=${id}`)
 }
 
 // get all weapon 
 export const getWeaponList = () => {
-    return instance.get(`https://autumnfish.cn/api/lol/weapon`)
+    return instance.get(`weapon`)
 }
 
 
 // random background image
 export const getRandomImg = () => {
-    return instance.get(`https://autumnfish.cn/api/cover/random`)
+    return axios.get(`https://autumnfish.cn/api/cover/random`)
 }
 
 // text to voice
 export const textToVoice = (text: string) => {
-    return instance.get(`http://www.liulongbin.top:3006/api/synthesize?text=${text}`)
+    return axios.get(`http://www.liulongbin.top:3006/api/synthesize?text=${text}`)
 }
--- src/api/request.ts
+++ src/api/request.ts
@@ -1,24 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://api.shop.eduwork.cn/api',
+    baseURL: 'https://autumnfish.cn/api/lol',
     timeout: 5000
 })
-
-// // Request Interceptors
-// instance.interceptors.request.use(config => {
-//     return config;
-// }, err => {
-//     return Promise.reject(err)
-// })
-
-// Response Interceptors
-// instance.interceptors.response.use(result => {
-//     return result.data;
-// }, err => {
-//     return Promise.reject(err)
-// })
-
 
 export default instance
 
